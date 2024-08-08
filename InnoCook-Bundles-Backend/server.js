cba0c4db4d46bad995435120179756292ca832ce
yhsu25 <connie.yth@gmail.com>
2024-08-08 18:35:36 -0400

    Update server.js

--- InnoCook-Bundles-Backend/server.js
+++ InnoCook-Bundles-Backend/server.js
@@ -22,8 +22,8 @@ app.use(cors());
 //   };
 // 
 //   app.use(cors(corsOptions));
-// app.use(express.json({ extended: false }));
-app.use(express.json());
+app.use(express.json({ extended: false }));
+// app.use(express.json());
 
 // Define Routes
 
