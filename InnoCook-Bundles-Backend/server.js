4830be334c5595507b3d3f03db6a6279cf69d991
Timi Jacobs <149181843+timij90@users.noreply.github.com>
2024-08-12 15:42:12 -0400

    Update server.js

--- InnoCook-Bundles-Backend/server.js
+++ InnoCook-Bundles-Backend/server.js
@@ -9,34 +9,23 @@ const search = require('./routes/recipeRoutes');
 
 const app = express();
 
-
-
 // Connect Database
 connectDB();
 
 // Middleware
-// const corsOptions = {
-// 	origin: 'https://inno-cook-bundles-web-application-5zz6af8ou.vercel.app/',
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// 	allowedHeaders: ['Content-Type', 'Authorization']
-//   };
-// app.options('*', cors()); // Enable pre-flight requests for all routes
-// app.use(cors({
-// 	origin: "*",
-// 	methods: ["GET", "POST", "PUT", "DELETE"]
-// }));
 app.use(cors({
   origin: "https://inno-cook.vercel.app",  // Allow specific origin
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"] // Allowed headers
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 }));
-app.options('*', cors()); 
-// app.use(express.json({ extended: false }));
+
+// Handle preflight requests for all routes
+app.options('*', cors());
+
+// Body parser middleware
 app.use(express.json());
 
 // Define Routes
-
-// app.get("/", (req, res) => res.send("Express on Vercel")); // testing used
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 app.use('/api/recipes', search);
@@ -44,7 +33,6 @@ app.use('/api/recipes', search);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 
 module.exports = app;
 
