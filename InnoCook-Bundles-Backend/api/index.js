1408995d0e6dab96f74665ed7df32bf58e6e0f4f
Timi Jacobs <149181843+timij90@users.noreply.github.com>
2024-08-12 14:59:09 -0400

    Update index.js

--- InnoCook-Bundles-Backend/api/index.js
+++ InnoCook-Bundles-Backend/api/index.js
@@ -16,7 +16,6 @@ connectDB();
 // Middleware
 app.use(cors({origin:"*",
 	methods:["GET","POST","PUT","DELETE"],
-	allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.options('*', cors()); 
