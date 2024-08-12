921a02e337cbc04b6b6fdd5a8a2b0fa9e6483b95
Timi Jacobs <149181843+timij90@users.noreply.github.com>
2024-08-12 15:49:52 -0400

    Update auth.js

--- InnoCook-Bundles-Backend/api/auth.js
+++ InnoCook-Bundles-Backend/api/auth.js
@@ -4,15 +4,21 @@ const express = require('express');
 const { register, login } = require('../controllers/authController');
 const cors = require('cors');
 
-
 const app = express();
+
+// CORS Configuration
 app.use(cors({
-	origin: "*",
-	methods: ["GET", "POST", "PUT", "DELETE"]
-  }));
+  origin: "https://inno-cook.vercel.app",  // Allow specific origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
+
+// Body parser middleware
 app.use(express.json());
 
+// Define Routes
 app.post('/api/auth/register', register);
 app.post('/api/auth/login', login);
 
 module.exports = app;
+
