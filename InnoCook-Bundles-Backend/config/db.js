b77a3263b4e8034bf9657c5a1e80af08bde76ce6
yhsu25 <connie.yth@gmail.com>
2024-08-13 16:02:59 -0400

    Update db.js

--- InnoCook-Bundles-Backend/config/db.js
+++ InnoCook-Bundles-Backend/config/db.js
@@ -16,10 +16,8 @@
 //
 // module.exports = connectDB;
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -28,16 +26,16 @@ const client = new MongoClient(process.env.MONGODB_URI, {
     }
 });
 
-async function run() {
+const connectDB = async () => {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);  // Exit process with failure
     }
-}
-run().catch(console.dir);
+};
+
+const getDB = (dbName) => client.db(dbName);
+
+module.exports = { connectDB, getDB };
