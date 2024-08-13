86d2dc2b2f1bcd83fbff6e4d31ae976910d335da
yhsu25 <connie.yth@gmail.com>
2024-08-13 16:42:21 -0400

    Update db.js

--- InnoCook-Bundles-Backend/config/db.js
+++ InnoCook-Bundles-Backend/config/db.js
@@ -1,41 +1,41 @@
-// // /innocook-backend/config/db.js
-// const mongoose = require('mongoose');
-//
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI, {
-//             // useNewUrlParser: true,
-//             // useUnifiedTopology: true
-//         });
-//         console.log('MongoDB connected');
-//     } catch (err) {
-//         console.error(err.message);
-//         process.exit(1);
-//     }
-// };
-//
-// module.exports = connectDB;
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const client = new MongoClient(process.env.MONGODB_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// /innocook-backend/config/db.js
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await client.connect();
+        await mongoose.connect(process.env.MONGODB_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
+        });
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
-        process.exit(1);  // Exit process with failure
+        process.exit(1);
     }
 };
 
-const getDB = (dbName) => client.db(dbName);
+module.exports = connectDB;
 
-module.exports = { connectDB, getDB };
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// 
+// const client = new MongoClient(process.env.MONGODB_URI, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
+// 
+// const connectDB = async () => {
+//     try {
+//         await client.connect();
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);  // Exit process with failure
+//     }
+// };
+// 
+// const getDB = (dbName) => client.db(dbName);
+// 
+// module.exports = { connectDB, getDB };
