84052373af203e252728591b2fee50310377e71b
Timi Jacobs <149181843+timij90@users.noreply.github.com>
2024-08-13 01:16:16 -0400

    Update authRoutes.js

--- InnoCook-Bundles-Backend/routes/authRoutes.js
+++ InnoCook-Bundles-Backend/routes/authRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/test', (req, res) => res.send('Auth route is working'));
 
 module.exports = router;
