8f6a0a62d1a6562a4f95c3d1e0dc1d8cf398acb0
yhsu25 <connie.yth@gmail.com>
2024-08-13 15:55:56 -0400

    Update db.js

--- InnoCook-Bundles-Backend/config/db.js
+++ InnoCook-Bundles-Backend/config/db.js
@@ -1,17 +1,43 @@
-// /innocook-backend/config/db.js
-const mongoose = require('mongoose');
+// // /innocook-backend/config/db.js
+// const mongoose = require('mongoose');
+//
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             // useNewUrlParser: true,
+//             // useUnifiedTopology: true
+//         });
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// };
+//
+// module.exports = connectDB;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-};
+});
 
-module.exports = connectDB;
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
