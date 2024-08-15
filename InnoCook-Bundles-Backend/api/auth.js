b162dd7ee64b2bd6a5b65595b4b9aaa713204614
yhsu25 <connie.yth@gmail.com>
2024-08-15 12:04:10 -0400

    test

--- InnoCook-Bundles-Backend/api/auth.js
+++ InnoCook-Bundles-Backend/api/auth.js
@@ -2,10 +2,23 @@
 
 const express = require('express');
 const { register, login } = require('../controllers/authController');
-const router = express.Router();
+const cors = require('cors');
 
-router.post('/register', register);
-router.post('/login', login);
-// router.get('/test', (req, res) => res.send('Auth route is working'));
+const app = express();
 
-module.exports = router;
+// CORS Configuration
+app.use(cors({
+  origin: "https://inno-cook.vercel.app",  // Allow specific origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+}));
+
+// Body parser middleware
+app.use(express.json());
+
+// Define Routes
+app.post('/api/auth/register', register);
+app.post('/api/auth/login', login);
+
+module.exports = app;
+
--- InnoCook-Bundles-Backend/api/recipes.js
+++ InnoCook-Bundles-Backend/api/recipes.js
@@ -3,10 +3,15 @@
 const express = require('express');
 const { searchRecipes, searchRecipeById } = require('../controllers/recipeController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
+const cors = require('cors');
 
+const app = express();
+app.use(cors({origin:"*",
+	methods:["GET","POST","PUT","DELETE"]
+}));
+app.use(express.json());
 
-router.post('/recipes/search', searchRecipes);
-router.post('/recipes/search/id', authMiddleware, searchRecipeById);
+app.post('/api/recipes/search', searchRecipes);
+app.post('/api/recipes/search/id', authMiddleware, searchRecipeById);
 
-module.exports = router
+module.exports = app;
