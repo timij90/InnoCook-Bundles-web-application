b46106ca2cdfff120d2923535e1bba930417ff7d
yhsu25 <connie.yth@gmail.com>
2024-08-15 11:59:18 -0400

    Update db.js

--- InnoCook-Bundles-Backend/config/db.js
+++ InnoCook-Bundles-Backend/config/db.js
@@ -1,51 +1,45 @@
 // /innocook-backend/config/db.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// 
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log('MongoDB connected');
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// };
+// 
+// module.exports = connectDB;
 
-let isConnected; // Track the connection status
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 const connectDB = async () => {
-    if (isConnected) {
-        console.log('Using existing database connection');
-        return;
-    }
-    
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        isConnected = db.connections[0].readyState;
-        console.log('MongoDB connected');
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
+        console.error(err.message);
         process.exit(1);
     }
 };
 
 module.exports = connectDB;
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// 
-// const client = new MongoClient(process.env.MONGODB_URI, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-// 
-// const connectDB = async () => {
-//     try {
-//         await client.connect();
-//         console.log('MongoDB connected');
-//     } catch (err) {
-//         console.error(err.message);
-//         process.exit(1);  // Exit process with failure
-//     }
-// };
-// 
-// const getDB = (dbName) => client.db(dbName);
-// 
-// module.exports = { connectDB, getDB };
