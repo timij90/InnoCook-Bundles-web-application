6223d5dd40a2895c1fc16fd85785dc32fa6d6746
yhsu25 <connie.yth@gmail.com>
2024-08-15 11:12:12 -0400

    update route setting

--- InnoCook-Bundles-Backend/api/recipes.js
+++ InnoCook-Bundles-Backend/api/recipes.js
@@ -3,15 +3,10 @@
 const express = require('express');
 const { searchRecipes, searchRecipeById } = require('../controllers/recipeController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const cors = require('cors');
+const router = express.Router();
 
-const app = express();
-app.use(cors({origin:"*",
-	methods:["GET","POST","PUT","DELETE"]
-}));
-app.use(express.json());
 
-app.post('/api/recipes/search', searchRecipes);
-app.post('/api/recipes/search/id', authMiddleware, searchRecipeById);
+router.post('/recipes/search', searchRecipes);
+router.post('/recipes/search/id', authMiddleware, searchRecipeById);
 
-module.exports = app;
+module.exports = router
