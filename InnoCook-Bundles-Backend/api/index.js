3daf90a08681cc1d6c0dd5e19f906b10959b14f6
yhsu25 <connie.yth@gmail.com>
2024-08-18 23:25:37 -0400

    test - old version deployment

--- InnoCook-Bundles-Backend/api/index.js
+++ InnoCook-Bundles-Backend/api/index.js
@@ -1,5 +1,43 @@
 // /innocook-backend/server.js
 // require('dotenv').config();
+// const express = require('express');
+// const connectDB = require('../config/db');
+// const cors = require('cors');
+// const users = require('../routes/userRoutes');
+// const auth = require('../routes/authRoutes');
+// const search = require('../routes/recipeRoutes');
+// 
+// const app = express();
+// 
+// 
+// // Connect Database
+// connectDB();
+// 
+// // Middleware
+// app.use(cors());
+// 
+// // app.use(express.json({ extended: false }));
+// app.use(express.json());
+// 
+// // Define Routes
+// 
+// app.get("/", (req, res) => res.send("Express on Vercel")); // testing used
+// app.use('/auth', auth);
+// app.use('/users', users);
+// app.use('/recipes', search);
+// 
+// const PORT = process.env.PORT || 8080;
+// 
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// // Example route
+// app.get('/api/hello', (req, res) => {
+// 	res.json({ message: 'Hello from the server!' });// testing used
+//   });
+//   
+// module.exports = app; 
+
+// /innocook-backend/server.js
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('../config/db');
 const cors = require('cors');
@@ -9,12 +47,12 @@ const search = require('../routes/recipeRoutes');
 
 const app = express();
 
+app.use(cors());
 
 // Connect Database
 connectDB();
 
 // Middleware
-app.use(cors());
 
 // app.use(express.json({ extended: false }));
 app.use(express.json());
